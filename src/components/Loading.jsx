d279311579bbb3dec55988f05e99967807695099
John O'Halloran <john.ohalloran@telus.com>
2023-01-19 11:06:41 -0500

    add Panels component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,12 @@
-import React, { Component } from "react"; // External Imports
+// ------------- External Imports ------------- //
+import React, { Component } from "react";
 
-export default class Loading extends Component {
- render() {
-  return <section className="loading">Loading</section>;
- }
-}
+
+class Loading extends Component {
+  render() {
+    return <section className="loading">Loading</section>;
+  };
+};
+
+
+export default Loading;
